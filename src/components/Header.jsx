22eace9c258e589d18f216eef4784ec6d37eb3a5
myusufadiyaman <49249673+MUHAMMED-YUSUF-ADIYAMAN@users.noreply.github.com>
2022-02-05 22:25:13 +0300

    commit tree 9c1c3eff3bebb947a4b55b39b7cdd411b1214838

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header({isDarkMode,handleDarkModeClick}) {
+    return  
+   ( <><header>
+      <h2>Shopster</h2>
+      <button onClick={handleDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </header></>);
+  }
+  
+  export default Header;
